67aa19028ad14e761b4e5e0a32115c60c872ac6e
Ran Yan <rayan@f1r1s8.codam.nl>
2026-08-13 17:37:34 +0200

    ex3 in process

--- module_2/ex3/ft_custom_errors.py
+++ module_2/ex3/ft_custom_errors.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
 
-def 
+class GardenError(Exception):
+    pass
+
+
+class PlantError(GardenError):
+    pass
+
+
+class WaterError(GardenError):
+    pass
+
+
+#check whether the plants' health violates the defined rule -- raise PlantError
+def check_plant_health(health: int) -> None:
+    if health < 0:
+        raise PlantError
+    else:
+        return
+
+
+def test_case() -> None:
+    
+    
+
 
 def main() -> None:
+    print("=== Custom Garden Errors Demo ===")
+    print()
+    test_case()
 
 
 if __name__ == "__main__":
-	main()
+    main()

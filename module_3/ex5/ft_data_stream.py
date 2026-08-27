0a90cd42624ee796ff55eb7da8ab2f17aa78cced
Ran <ran.yan.v@outlook.com>
2026-08-28 00:59:18 +0200

    ex5 ex6 done

--- module_3/ex5/ft_data_stream.py
+++ module_3/ex5/ft_data_stream.py
@@ -1,11 +1,45 @@
 #!/usr/bin/env python3
 
 import typing
+import random
 
+
+def gen_event() -> typing.Generator[tuple[str, str], None, None]:
+    players = ["xiaohei", "shanxin", "xiaobai", "gege", "nezha", "wuxian"]
+    activities = ["walk", "eat", "sleep", "attack", "climb", "defend", "heal"]
+    while True:
+        name = random.choice(players)
+        action = random.choice(activities)
+        yield (name, action)
+
+
+def consume_event(
+    records: list
+) -> typing.Generator[tuple[str, str], None, None]:
+    while records:
+        out = random.choice(records)
+        records.remove(out)
+        yield out
 
 
 def main() -> None:
-    print("")
+    print("=== Game Data Stream Processor ===")
+    generator = gen_event()
+    for index in range(1000):
+        event = next(generator)
+        print(f"Event {index}: Player {event[0]} did action {event[1]}")
+
+    generator = gen_event()
+    records = []
+    for index in range(10):
+        records.append(next(generator))
+    print(f"Built list of {len(records)} events: {records}")
+
+    new_records = consume_event(records)
+    for out in new_records:
+        print(f"Got event from list: {out}")
+        print(f"Remains in list: {records}")
+
 
 if __name__ == "__main__":
     main()
--- module_3/ex6/ft_data_alchemist.py
+++ module_3/ex6/ft_data_alchemist.py
@@ -4,7 +4,37 @@ import random
 
 
 def main() -> None:
-    print("")
+    print("=== Game Data Alchemist ===\n")
+
+    players = [
+        'Alice', 'bob', 'Charlie', 'dylan', 'Emma',
+        'Gregory', 'john', 'kevin', 'Liam'
+    ]
+    print(f"Initial list of players: {players}")
+
+    players_1 = [player.capitalize() for player in players]
+    players_2 = [player for player in players if player.capitalize() == player]
+    print(f"New list with all names capitalized: {players_1}")
+    print(f"New list of capitalized names only: {players_2}")
+    print()
+
+    scores = {
+        player: random.randint(0, 100)
+        for player in players_1
+    }
+    print(f"Score dict: {scores}")
+
+    total_score = sum(scores.values())
+    average_score = round(total_score / len(scores.values()), 2)
+    print(f"Score average is {average_score}")
+
+    high_scores = {
+        player: score
+        for player, score in scores.items()
+        if score > average_score
+    }
+    print(f"High scores: {high_scores}")
+
 
 if __name__ == "__main__":
     main()

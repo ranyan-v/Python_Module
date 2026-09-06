1fdab63c608550c38ddca09eea3b2d05f5e03bbe
Ran Yan <rayan@f0r1s14.codam.nl>
2026-09-06 19:31:10 +0200

    ex0

--- module_5/ex0/data_processor.py
+++ module_5/ex0/data_processor.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 
+import typing
+import abc
+
+
+class DataProcessor(ABC):
+
+    @abstactmethod
+    def validate():
+
+    @abstactmethod
+    def ingest():
+
+    normal output()
+
+class NumericProcessor(ABC):
+
+
+class TextProcessor(ABC):
+
+
+class LogProcessor(ABC):
+    
+
+
 def main() -> None:
     print()
 
